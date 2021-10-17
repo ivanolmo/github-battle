dc8e3b19679e21ebfa80dc6accdbd6864fc4c0ed
Ivan Olmo <ivan.olmo@gmail.com>
2021-10-17 18:05:00 -0500

    Add PlayerInput component to Battle component. Add minor css change to component.

--- app/Components/Battle.js
+++ app/Components/Battle.js
@@ -64,7 +64,7 @@ class PlayerInput extends React.Component {
             id='username'
             className='input-light'
             placeholder='github username'
-            autocomplete='off'
+            autoComplete='off'
             value={this.state.username}
             onChange={this.handleChange}
           />
@@ -87,10 +87,48 @@ PlayerInput.propTypes = {
 };
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player,
+    });
+    console.log(this.state);
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state;
+
     return (
       <React.Fragment>
         <Instructions />
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+        </div>
+        <div className='row space-around'>
+          {playerOne === null && (
+            <PlayerInput
+              label='Player One'
+              onSubmit={(player) => this.handleSubmit('playerOne', player)}
+            />
+          )}
+
+          {playerTwo === null && (
+            <PlayerInput
+              label='Player Two'
+              onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+            />
+          )}
+        </div>
       </React.Fragment>
     );
   }
