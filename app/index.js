b6eea37b2df8f36cfa05b46d1c764a456ada5481
Ivan Olmo <ivan.olmo@gmail.com>
2021-10-17 12:47:51 -0500

    Remove code used to test render. Add single component to container div.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import data from './data';
 import './index.css';
+import Popular from './Components/Popular';
 
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    );
+    // let words = data.split(' ');
+    // return (
+    //   <ul>
+    //     {words.map((word) => (
+    //       <li>{word} what</li>
+    //     ))}
+    //   </ul>
+    // );
   }
 }
 
