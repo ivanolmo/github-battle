52fae1fd8a74a66629b70c7662d3bc3212e09de0
Ivan Olmo <ivan.olmo@gmail.com>
2021-10-17 21:14:14 -0500

    Add full UI for battle results screen.

--- app/Components/Results.js
+++ app/Components/Results.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  FaCompass,
+  FaBriefcase,
+  FaUsers,
+  FaUser,
+  FaUserFriends,
+  FaCode,
+} from 'react-icons/fa';
 import { battle } from '../utils/api';
 
 export default class Results extends React.Component {
@@ -34,10 +42,104 @@ export default class Results extends React.Component {
   }
 
   render() {
+    const { winner, loser, error, loading } = this.state;
+
+    if (loading === true) {
+      return <p>LOADING</p>;
+    }
+
+    if (error) {
+      return <p className='center-text error'>{error}</p>;
+    }
+
     return (
-      <div>
-        Results
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      <div className='grid space-around container-small'>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === loser.score ? 'Tie' : 'Winner'}
+          </h4>
+          <img
+            src={winner.profile.avatar_url}
+            alt={`Avatar for Winner ${winner.profile.login}`}
+            className='avatar'
+          />
+          <h2 className='center-text'>
+            <a href={winner.profile.html_url} className='link'>
+              {winner.profile.login}
+            </a>
+          </h2>
+          <h4 className='center-text'>
+            Score: {winner.score.toLocaleString()}
+          </h4>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {winner.profile.name}
+            </li>
+            {winner.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {winner.profile.location}
+              </li>
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {winner.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {winner.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </div>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === loser.score ? 'Tie' : 'Loser'}
+          </h4>
+          <img
+            src={loser.profile.avatar_url}
+            alt={`Avatar for Loser ${loser.profile.login}`}
+            className='avatar'
+          />
+          <h2 className='center-text'>
+            <a href={loser.profile.html_url} className='link'>
+              {loser.profile.login}
+            </a>
+          </h2>
+          <h4 className='center-text'>Score: {loser.score.toLocaleString()}</h4>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {loser.profile.name}
+            </li>
+            {loser.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {loser.profile.location}
+              </li>
+            )}
+            {loser.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {loser.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {loser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {loser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
