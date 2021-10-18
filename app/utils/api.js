57c1e5cd02283ae63b78fec3f288854f7515989a
Ivan Olmo <ivan.olmo@gmail.com>
2021-10-18 17:51:18 -0500

    Fix error in fetch URL.

--- app/utils/api.js
+++ app/utils/api.js
@@ -61,10 +61,9 @@ export function battle(players) {
 }
 
 export function fetchPopularRepos(language) {
-  const endpoint =
-    window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:
-  ${language}&sort=stars&order=desc&type=Repositories`);
-
+  const endpoint = window.encodeURI(
+    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+  );
   return fetch(endpoint)
     .then((res) => res.json())
     .then((data) => {
