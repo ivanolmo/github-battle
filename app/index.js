fd538f8bae2785f231c8a9e81502cbd5e334000c
Ivan Olmo <ivan.olmo@gmail.com>
2021-11-09 12:14:31 -0600

    Add React Context to project.

--- app/index.js
+++ app/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './Components/Popular';
+import Battle from './Components/Battle';
+
+import { ThemeProvider } from './contexts/theme';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light',
+        }));
+      },
+    };
+  }
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Popular />
+            {/* <Battle /> */}
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
