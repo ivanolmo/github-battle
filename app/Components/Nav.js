d9a0b4e28f72d02e26299fdf91cfffffeecfed14
Ivan Olmo <ivan.olmo@gmail.com>
2021-12-08 19:32:33 -0600

    Update Nav component with links to popular/battle pages.

--- app/Components/Nav.js
+++ app/Components/Nav.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { ThemeConsumer } from '../contexts/theme';
+
+const isActive = ({ isActive }) => ({ color: isActive && 'rgb(187, 46, 31)' });
 
 export default function Nav() {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className='row space-between'>
+          <ul className='row nav'>
+            <li>
+              <NavLink to='/' style={isActive} className='nav-link'>
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/battle' style={isActive} className='nav-link'>
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             className='btn-clear'
             style={{ fontSize: 30 }}
